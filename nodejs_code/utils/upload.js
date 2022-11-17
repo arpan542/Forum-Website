b0544e8e58838268b22c6aef4d3e84c2ee9cc4e1
arpan542 <117260057+arpan542@users.noreply.github.com>
2022-11-17 11:18:51 +0530

    Add files via upload

--- nodejs_code/utils/upload.js
+++ nodejs_code/utils/upload.js
@@ -1,6 +1,9 @@
 import multer from 'multer';
 import { GridFsStorage } from 'multer-gridfs-storage';
+import dotenv from 'dotenv';
 
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
 const storage = new GridFsStorage({
     url: `mongodb://${username}:${password}@ac-ncsdx4f-shard-00-00.pnqxrcx.mongodb.net:27017,ac-ncsdx4f-shard-00-01.pnqxrcx.mongodb.net:27017,ac-ncsdx4f-shard-00-02.pnqxrcx.mongodb.net:27017/?ssl=true&replicaSet=atlas-lf2cpn-shard-0&authSource=admin&retryWrites=true&w=majority`,
     options: { useNewUrlParser: true },
